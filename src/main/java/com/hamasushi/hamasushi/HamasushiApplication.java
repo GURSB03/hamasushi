3067c1f125afd08d283759b768821b51a439333c
2Gursb03 <smile121730@gmail.com>
2026-09-19 10:58:42 +0900

    Initial commit

---
+++ src/main/java/com/hamasushi/hamasushi/HamasushiApplication.java
@@ -0,0 +1,13 @@
+package com.hamasushi.hamasushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamasushiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamasushiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamasushi/hamasushi/HamasushiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamasushi.hamasushi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamasushiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
